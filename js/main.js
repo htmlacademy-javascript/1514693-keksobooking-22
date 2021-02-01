72de0edf025b6ea3e38f7958c270bff837953cf2
mihail-salihov <salihov.mr@mail.ru>
2021-02-01 21:45:07 +0300

    2.Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
+// метод получения случайного числа  из диапазона https://learn.javascript.ru/task/random-int-min-max
+
 function randomInteger(min, max) {
-  // метод получения случайного числа  из диапазона https://learn.javascript.ru/task/random-int-min-max
   let rand = min + Math.random() * (max + 1 - min);
   if (min >= max) {
-    return 'Ошибка';
+    throw new Error('Число min не должно превышать max');
   }
   return Math.floor(rand);
 }
@@ -12,12 +13,13 @@ alert(randomInteger(0, 10));
 
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+// метод округления https://yandex.ru/turbo/internet-technologies.ru/s/articles/metody-okrugleniya-chisel-v-javascript.html
+
 function randomFractional (min, max) {
   let rand = min + Math.random() * (max + 1 - min);
-  // метод округления https://yandex.ru/turbo/internet-technologies.ru/s/articles/metody-okrugleniya-chisel-v-javascript.html
   let rounded = parseFloat(rand.toFixed(2));
   if (min >= max) {
-    return 'Ошибка';
+    throw new Error('Число min не должно превышать max');
   }
   return rounded;
 }
